d87730ee401d9e238a40ddd0679c019114926ee3
Ni4ka1991 <astahova_nica@mail.ru>
2021-05-07 20:44:35 +0300

    Add file app.py

--- app.py
+++ app.py
@@ -31,11 +31,27 @@ class Water:
     def __len__( self ):
         return self.volume
 
+## comparation func ##################
+
     def __gt__ ( self, other ):
         if self.isNumber( other ):
             return self.volume > other
         else:
             return self.volume > other.volume
+    
+    def __eq__ ( self, other ):
+        if self.isNumber( other ):
+            return self.volume == other
+        else:
+            return self.volume == other.volume
+    
+    def __ne__ ( self, other ):
+        if self.isNumber( other ):
+            return self.volume != other
+        else:
+            return self.volume != other.volume
+    
+## ########################################    
     
     def __add__ ( self, other ):
         return Water( self.volume + other.volume )
@@ -46,15 +62,20 @@ class Water:
 
 emptyWater = Water()
 largeWater = Water( "large" )
+largeWater2 = Water( 1000 )
 smallWater = Water( 10 )
 resultWater = largeWater + smallWater
 
 
 system( "clear" )
-print( emptyWater ) 
-print( largeWater )
-print( smallWater )
-print( resultWater )
-print( largeWater > smallWater )
+#print( emptyWater ) 
+#print( largeWater )
+#print( smallWater )
+#print( resultWater )
+
+print("COMPARISON RESULTS:")
+print( f"Is Large Water < Small Water? >>> {largeWater < smallWater}" )
+print( f"Is Large Water 2 == Large Water? >>> {largeWater2 == largeWater}" )
+print( f"Is Large Water 2 != Large Water? >>> {largeWater2 != largeWater}" )
 
 
