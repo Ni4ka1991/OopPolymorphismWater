11f2b86562cd83f6c278d96b6262e27a4db517a6
Ni4ka1991 <astahova_nica@mail.ru>
2021-05-07 22:53:44 +0300

    Add file app.py app-s-ns.py

--- app-s-ns.py
+++ app-s-ns.py
@@ -2,39 +2,74 @@
 
 from os import system
 
+###################### CLASS DESCRIPTION #########################
+
 class Water:
     
     def __init__ ( self, volume = 1, temp = 18 ):
-        self.volume  = volume
-        self.temp    = temp
-        if( temp > 100 ):
-            self.state   = "vapor"
-        elif( temp <= 0 ):
-            self.state = "solid"
-        else:
-            self.state = "liquid"
+        self.set_volume( volume )
+        self.set_temp( temp )
 
     def __str__ ( self ):
-        return f"WATER <{self.volume}> L | <{self.temp}> C | <{self.state}> "
-################################################################
+        return f"WATER <{self.__volume}> L | <{self.__temp}> C | <{self.__state}> "
+
+## setter & getter for temp ###########
+    
+    def set_temp( self, temp ):
+        if temp > 2000 or temp < -273:
+            print( "ERROR!" )
+        else:
+            self.__temp = temp
+            if( temp > 100 ):
+                self.__state   = "vapor"
+            elif( temp <= 0 ):
+                self.__state = "solid"
+            else:
+                self.__state = "liquid"
+
+    
+    def get_temp( self ):
+        return self.__temp
+## #################################
+
+## setter & getter for volume ###########
+    
+    def set_volume( self, volume ):
+        if self.isNumber( volume ):
+            if ( volume <= 0 ):
+                print( "ERROR!!" )
+            else:
+                self.__volume  = volume
+
+        elif ( type( volume ) == str ) and volume in [ "large", "medium", "small" ]:
+                if( volume == "large" ):
+                    self.__volume = 1000
+            
+                elif( volume == "medium" ):
+                    self.__volume = 100
+            
+                elif( volume == "small" ):
+                    self.__volume = 10
+        
+    
+    def get_volume( self ):
+        return self.__volume
+## #################################
+
+    def isNumber( self, value ):
+        return ( type( value ) == int or type( value ) == float )
+###################### CLASS DESCRIPTION #########################
 
 
-def heet( water, deltaTemp = 0 ):
-    result = Water( water.volume, water.temp + deltaTemp )
-    return result
-
-def cool( water, deltaTemp = 0 ):
-    result = Water( water.volume, water.temp - deltaTemp )
-    return result
-
-
-
+def heet( water, value, deltaTemp = 0 ):
+    water.set_volume( value )
+    water.set_temp( water.get_temp() + deltaTemp ) 
+    print( f"{water.get_temp()} C" )
+    print( f"{water.get_volume()} L" )
 
 
 w = Water()
-w = heet( w, 100 )
-w = cool( w, 150 )
 system( "clear" )
-print(w)
+heet( w, 234, 30 )
 
 
